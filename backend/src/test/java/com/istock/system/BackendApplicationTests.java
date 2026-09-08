ef953720c804f7db726182d4899511ec20a64566
Cadu-f12 <cadudailha68@gmail.com>
2026-09-08 20:19:51 -0300

    refactor: Refactor the infraestructure and modularize it

--- backend/src/test/java/com/istock/system/BackendApplicationTests.java
+++ backend/src/test/java/com/istock/system/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.istock;
+package com.istock.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ManagerApplicationTests {
+class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
